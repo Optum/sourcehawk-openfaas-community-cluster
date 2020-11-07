ba7079b67b2f7b8f58a4c3c5154a7b113cd523c0
brianwyka <brian.wyka@gmail.com>
2020-11-07 12:37:45 -0800

    Write all files to /tmp prefix

--- scan/function/scan.sh
+++ scan/function/scan.sh
@@ -35,9 +35,10 @@ set -e
 ###############################################################################
 
 UUID=$(date +%s%N | cut -b1-13)
-ZIP_FILE="${GITHUB_ORG}-${GITHUB_REPO}-${GITHUB_REF}-$UUID.zip"
-SOURCE_CODE_EXTRACT_DIRECTORY="${GITHUB_ORG}-${GITHUB_REPO}-${GITHUB_REF}-$UUID"
-PROVIDED_CONFIG_FILE="/tmp/sourcehawk-provided.yml"
+TEMP_DIR_PREFIX="/tmp/${GITHUB_ORG}-${GITHUB_REPO}-${GITHUB_REF}-$UUID"
+ZIP_FILE="$TEMP_DIR_PREFIX/source.zip"
+SOURCE_CODE_EXTRACT_DIRECTORY="$TEMP_DIR_PREFIX/source"
+PROVIDED_CONFIG_FILE="$TEMP_DIR_PREFIX/sourcehawk-provided.yml"
 
 error_and_exit() {
   echo "$2" > /dev/stderr
@@ -51,11 +52,9 @@ cleanup() {
 trap cleanup INT EXIT
 
 # Retrieve the provided config from stdin
-if read -t 0; then
+if [ -p /dev/stdin ]; then
   cat > "$PROVIDED_CONFIG_FILE"
 fi
-
->&2 echo "got here 1: $(pwd)"
 
 # Download a zip file of the repository contents
 GITHUB_ZIPBALL_URL="$GITHUB_API_URL/repos/$GITHUB_ORG/$GITHUB_REPO/zipball/$GITHUB_REF"
@@ -65,7 +64,7 @@ if [ -z "${GITHUB_AUTH_TOKEN}" ]; then
 else
   CURL_RESPONSE_CODE=$(curl -L -s -w "%{http_code}" -H "Authorization: token $GITHUB_AUTH_TOKEN" "$GITHUB_ZIPBALL_URL" -o "$ZIP_FILE" || error_and_exit 65 "Error downloading/writing zip file")
 fi
->&2 echo "curl completed: $CURL_RESPONSE_CODE"
+
 if [ "$CURL_RESPONSE_CODE" = 401 ]; then
   if [ -z "${GITHUB_AUTH_TOKEN}" ]; then
     error_and_exit 61 "Are you trying to scan a non-public repository? An authorization token is required."
@@ -76,18 +75,12 @@ elif [ "$CURL_RESPONSE_CODE" = 404 ]; then
   error_and_exit 64 "Could not find source code to scan.  If the repository requires access, make sure to provide an Authorization token"
 fi
 
->&2 echo "got here 2"
-
 # Unzip the source code tarball
 mkdir -p "$SOURCE_CODE_EXTRACT_DIRECTORY" && unzip -qq -d "$SOURCE_CODE_EXTRACT_DIRECTORY" "$ZIP_FILE"
-
->&2 echo "got here 3"
 
 # Grab the repository root from unzipped tarball
 # shellcheck disable=SC2035
 SOURCE_CODE_ROOT_DIRECTORY=$(cd "$SOURCE_CODE_EXTRACT_DIRECTORY" && cd */. && pwd)
-
->&2 echo "got here 4"
 
 # Use the provided config file if present, otherwise default
 CONFIG_FILE="${SOURCE_CODE_ROOT_DIRECTORY}/sourcehawk.yml"
@@ -95,12 +88,8 @@ if [ -f "$PROVIDED_CONFIG_FILE" ]; then
   CONFIG_FILE="$PROVIDED_CONFIG_FILE"
 fi
 
->&2 echo "got here 5"
-
 # Execute the scan
 ./function/sourcehawk scan --verbosity MEDIUM --output-format "$OUTPUT_FORMAT" --config-file "$CONFIG_FILE" "$SOURCE_CODE_ROOT_DIRECTORY"
-
->&2 echo "got here 6"
 
 # Cleanup everything
 cleanup
